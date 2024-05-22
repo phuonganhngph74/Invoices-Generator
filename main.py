3b44e5865062814bc2e4925badbc7121e8d94e71
phanhphanh7400 <nguyen.ppa740@gmail.com>
2024-05-22 22:12:28 +0200

    Add header, total price, company

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ for filepath in filepaths:
     invoice_nr, date = filename.split('-')
 
     pdf.set_font("Times", size=16, style='B')
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}",ln=1)
+    pdf.cell(w=50, h=8, txt=f"Invoice Nr.{invoice_nr}", ln=1)
 
     pdf.set_font("Times", size=16, style='B')
     pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
@@ -34,11 +34,27 @@ for filepath in filepaths:
     # Add the data rows to the table
     for index, row in df.iterrows():
         pdf.set_font(family="Times", size=10)
-        pdf.set_text_color(80,80,80)
+        pdf.set_text_color(80, 80, 80)
         pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
         pdf.cell(w=70, h=8, txt=str(row['product_name']), border=1)
         pdf.cell(w=33, h=8, txt=str(row['amount_purchased']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+
+    # Add the total sum
+    total_sum = df['total_price'].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.set_x(203 - 30)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Add total sum sentence
+    pdf.set_font(family="Times", size=14, style='B')
+    pdf.cell(w=30, h=10, txt=f'The total price is: {total_sum}', ln=1)
+
+    # Add logo and company name
+    pdf.set_font(family="Times", size=14, style='B')
+    pdf.cell(w=30, h=8, txt=f'PythonHow')
+    pdf.image('images.png', x=37, y=67, w=10)
 
     pdf.output(f"PDFs/{filename}.pdf")
